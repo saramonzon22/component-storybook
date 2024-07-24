46766d052d57c25e62ac203c99f21579cfed87b2
Sara Monzón <sara.mon.quesada@gmail.com>
2024-07-24 11:38:07 +0100

    initial commit

---
+++ .storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from "@storybook/web-components-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-mdx-gfm"
+  ],
+
+  framework: {
+    name: "@storybook/web-components-vite",
+    options: {},
+  },
+
+  docs: {}
+};
+export default config;
---
+++ src/stories/button/Button.stories.ts
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { fn } from '@storybook/test';
+import type { ButtonProps } from './Button';
+import { Button } from './Button';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+const meta = {
+  title: 'Example/Button',
+  tags: ['autodocs'],
+  render: (args) => Button(args),
+  argTypes: {
+    backgroundColor: { control: 'color' },
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium', 'large'],
+    },
+  },
+  args: { onClick: fn() },
+} satisfies Meta<ButtonProps>;
+
+export default meta;
+type Story = StoryObj<ButtonProps>;
+
+export const Primary: Story = {
+  args: {
+    primary: true,
+    label: 'Button',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    label: 'Button',
+  },
+};
+
