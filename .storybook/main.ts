570fe69f8cd924f585e4929884f3abfecc08949d
Sara Monzón <sara.mon.quesada@gmail.com>
2024-07-25 12:28:38 +0100

    update version

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,17 @@
 import type { StorybookConfig } from "@storybook/web-components-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-
+  framework: '@storybook/web-components-vite',
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
-    "@storybook/addon-mdx-gfm"
   ],
-
-  framework: {
-    name: "@storybook/web-components-vite",
-    options: {},
+  async viteFinal(config, options) {
+    // Add your configuration here
+    return config;
   },
-
-  docs: {}
 };
+
 export default config;
